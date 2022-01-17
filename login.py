b7da6423577e4391a2a6fef769f848fac93ec743
aoirint <aoirint@gmail.com>
2022-01-18 05:09:51 +0900

    login cli

---
+++ login.py
@@ -0,0 +1,22 @@
+from nicotoolspy.cli.login import create_auth_cookies
+
+
+if __name__ == '__main__':
+  import argparse
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--mail_tel', type=str)
+  parser.add_argument('--password', type=str)
+  parser.add_argument('--cookie_file', type=str, default='cookies.pkl')
+
+  args = parser.parse_args()
+
+  mail_tel = args.mail_tel
+  password = args.password
+  cookie_file = args.cookie_file
+
+  login(
+    mail_tel=mail_tel,
+    password=password,
+    cookie_file=cookie_file,
+  )
--- nicotoolspy/cli/login.py
+++ nicotoolspy/cli/login.py
@@ -4,25 +4,16 @@ from nicotoolspy.login import create_auth_cookies
 from nicotoolspy.session import create_session
 
 
-if __name__ == '__main__':
-  import argparse
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--mail_tel', type=str)
-  parser.add_argument('--password', type=str)
-  parser.add_argument('--cookie_file', type=str, default='cookies.pkl')
-
-  args = parser.parse_args()
-
-  mail_tel = args.mail_tel
+def create_auth_cookies(
+  mail_tel: str = None,
+  password: str = None,
+  cookie_file: str = None,
+):
   if not mail_tel:
     mail_tel = input('Email or TEL: ')
 
-  password = args.password
   if not password:
     password = getpass()
-
-  cookie_file = args.cookie_file
 
   session = create_session()
 
